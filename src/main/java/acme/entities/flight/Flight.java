52da2f0104e4f1f1ddac205d1c58fd07c38c9d73
Javier Garcia Rodriguez <javgarciar@gmail.com>
2025-07-03 11:21:21 +0200

    fix: removed useless getFlightSummary method

--- src/main/java/acme/entities/flight/Flight.java
+++ src/main/java/acme/entities/flight/Flight.java
@@ -1,7 +1,6 @@
 
 package acme.entities.flight;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -95,22 +94,6 @@ public class Flight extends AbstractEntity {
 		FlightRepository repository = SpringHelper.getBean(FlightRepository.class);
 		Integer legs = repository.countLegs(this.getId());
 		return legs != null && legs > 0 ? legs - 1 : 0;
-	}
-
-	@Transient
-	public String getFlightSummary() {
-		String tag = this.tag;
-		String origin = this.getOriginCity();
-		String destination = this.getDestinationCity();
-		Date departure = this.getScheduledDeparture();
-		Date arrival = this.getScheduledArrival();
-
-		SimpleDateFormat dateTimeFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-
-		String depTime = departure != null ? dateTimeFormat.format(departure) : "??/??/???? ??:??";
-		String arrTime = arrival != null ? dateTimeFormat.format(arrival) : "??/??/???? ??:??";
-
-		return String.format("[%s] %s → %s | %s - %s", tag, origin, destination, depTime, arrTime);
 	}
 
 
